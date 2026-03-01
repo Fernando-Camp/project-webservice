e374fc68a2e053a5640114aa909acca8d7f1a57c
Fernando <fernandocgoveia@gmail.com>
2026-03-01 20:22:59 -0300

    User Insert endpoint

--- src/main/java/com/fernando/webservice/resources/UserResource.java
+++ src/main/java/com/fernando/webservice/resources/UserResource.java
@@ -4,11 +4,10 @@ import com.fernando.webservice.model.entities.User;
 import com.fernando.webservice.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -29,4 +28,16 @@ public class UserResource {
         return ResponseEntity.ok().body(userService.findById(id));
     }
 
+        @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        user = userService.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user); // Retorna 201 como HTTP status code
+    }
+
+//    @PostMapping - retorna 200 como HTTP status code
+//    public ResponseEntity<User> createUser(@RequestBody User user) {
+//        user = userService.insert(user);
+//        return ResponseEntity.ok().body(user);
+//    }
 }
