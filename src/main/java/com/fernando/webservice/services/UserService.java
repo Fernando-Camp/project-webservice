d4e656243072f499a415f245cc22db5b9f6c4559
Fernando <fernandocgoveia@gmail.com>
2026-03-02 00:02:41 -0300

    Exception Handling - delete

--- src/main/java/com/fernando/webservice/services/UserService.java
+++ src/main/java/com/fernando/webservice/services/UserService.java
@@ -2,10 +2,14 @@ package com.fernando.webservice.services;
 
 import com.fernando.webservice.model.entities.User;
 import com.fernando.webservice.repositories.UserRepository;
+import com.fernando.webservice.services.exceptions.DataBaseException;
 import com.fernando.webservice.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,8 +32,17 @@ public class UserService {
         return userRepository.save(user);
     }
 
-    public void  delete(Long id) {
-        userRepository.deleteById(id);
+    public void delete(Long id) {
+        User user = userRepository.getReferenceById(id);
+        try {
+            userRepository.delete(user);
+        }
+        catch (ObjectOptimisticLockingFailureException e) {
+            throw new ResourceNotFoundException(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DataBaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User user) {
